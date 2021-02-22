e99c98d739759d88df4e4df84da781f27f619a5b
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-22 01:00:10 -0500

    Initial Commit

---
+++ LandmarksSwiftUI/LandmarksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSwiftUIApp.swift
+//  LandmarksSwiftUI
+//
+//  Created by Andy Lochan on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
