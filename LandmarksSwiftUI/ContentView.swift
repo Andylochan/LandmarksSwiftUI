73ba01f56434ae25a71564a0298e8ec97b032db1
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-22 19:28:27 -0500

    Add basic landmark detail view

--- LandmarksSwiftUI/ContentView.swift
+++ LandmarksSwiftUI/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Unisphere")
+                    .font(.title)
+                HStack {
+                    Text("Flushing Meadows-Corona Park")
+                    Spacer()
+                    Text("New York")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About the Unisphere")
+                    .font(.title2)
+                Text("The sphere, which measures 140 feet high and 120 feet in diameter, was designed by Gilmore D. Clarke as part of his plan for the 1964 New York World's Fair.")
+            }
             .padding()
+            
+            Spacer()
+        }
     }
 }
 
