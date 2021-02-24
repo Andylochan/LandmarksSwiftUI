241f84b7d367dbfd25cb6be926d27ea1ba180753
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-23 23:21:26 -0500

    Add Category View

---
+++ LandmarksSwiftUI/Model/Profile.swift
@@ -0,0 +1,9 @@
+//
+//  Profile.swift
+//  LandmarksSwiftUI
+//
+//  Created by Andy Lochan on 2/23/21.
+//
+
+import Foundation
+
--- LandmarksSwiftUI/View/Helpers/MapView.swift
+++ LandmarksSwiftUI/View/Helpers/MapView.swift
@@ -23,7 +23,7 @@ struct MapView: View {
     private func setRegion(_ coordinate: CLLocationCoordinate2D) {
         region = MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
     }
 }
