3e6bf95f576ab6a3d888b1e74069ddf45942d231
Andy L <36180382+Andylochan@users.noreply.github.com>
2021-02-24 00:18:42 -0500

    Add Edit View / UI Controls

---
+++ LandmarksSwiftUI/View/Profiles/ProfileSummary.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileSummary.swift
+//  LandmarksSwiftUI
+//
+//  Created by Andy Lochan on 2/23/21.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                
+                Text("Notifications: \(profile.prefersNotifications ? "On" : "Off")")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
+    }
+}
